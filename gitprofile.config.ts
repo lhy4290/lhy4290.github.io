fdcb376ac491a2b3aaf78565551a54392461753f
Iris Liu <155805623+lhy4290@users.noreply.github.com>
2026-06-13 17:38:00 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -39,29 +39,35 @@ const CONFIG = {
       projects: [
         // ---- Special Projects 區區 ----
         {
-          title: '【Special Project】<br />Enterprise Decision Support System Combining Generative Artificial Intelligence and Constrained Optimization',
-          description: 'Solving enterprise logistics route problems.',
+          title: '【Special Project】 Enterprise Decision Support System Combining Generative Artificial Intelligence and Constrained Optimization',
+          description: 'Solving enterprise logistics route problems',
           imageUrl: 'https://github.com/lhy4290/mywebsitematerial/blob/main/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88/4.png?raw=true',
           link: 'https://drive.google.com/drive/folders/1lDnk9YRvKD0ediuuTrLIamEBUsVrDJmo?usp=drive_link',
         },
         {
-          title: '【Special Project】<br />Big Data Salary Prediction Simulation System',
-          description: 'Solving the problem of opaque job and salary information abroad.',
+          title: '【Special Project】 Big Data Salary Prediction Simulation System',
+          description: 'Solving the problem of opaque job and salary information abroad',
           imageUrl: 'https://github.com/lhy4290/mywebsitematerial/blob/main/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88/3.png?raw=true',
           link: 'https://drive.google.com/drive/folders/1AuniF2BGd4yOWWtZ2QE3SHsM9G0JeKd3?usp=drive_link',
         },
         // ---- Business Projects 區區 ----
         {
-          title: '【Business Project】<br />Startup proposal collection system',
-          description: '【Business Project】Ideas and creativity of fundraising systems.',
+          title: '【Business Project】 Startup proposal collection system',
+          description: '【Business Project】Ideas and creativity of fundraising systems',
           imageUrl: 'https://github.com/lhy4290/mywebsitematerial/blob/main/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88/2.png?raw=true',
           link: 'https://drive.google.com/file/d/1Dbycx6vHATuKa_WNb8NdEhke2upC2Wo-/view?usp=drive_link',
         },
         {
-          title: '【Business Project】<br />International Marketing Plan for Air Purifiers',
+          title: '【Business Project】 International Marketing Plan for Air Purifiers',
           description: 'Marketing strategies for selling products in overseas markets',
           imageUrl: 'https://github.com/lhy4290/mywebsitematerial/blob/main/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88/1.png?raw=true',
           link: 'https://drive.google.com/file/d/1Vk-mVr-Xiw0fYFXfRjzAwfELAjGqff2K/view?usp=drive_link',
+        },
+        {
+          title: '【Business Project】 Taiwan Bopiliao Specialty Products Project',
+          description: 'How to integrate history into products and make them more widely known?',
+          imageUrl: 'https://github.com/lhy4290/mywebsitematerial/blob/main/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88/5.png?raw=true',
+          link: 'https://drive.google.com/file/d/1BvThHDaFAaA4ky2wut8lV-rQ8qqWMOTD/view?usp=drive_link',
         },
       ],
     },
